edef20853c3d8e8fb660f078b9321807b4241e20
Mikołaj Soska <miki_2007@gazeta.pl>
2021-03-27 10:31:58 +0100

    Created utils file in model module.

---
+++ model/utils.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+
+
+class TimeDistributed(nn.Module):
+    def __init__(self, module):
+        super(TimeDistributed, self).__init__()
+        self.module = module
+
+    def forward(self, x):
+        x_reshape = x.contiguous().view(-1, x.size(-1))
+
+        y = self.module(x_reshape)
+        y = y.view(-1, x.size(1), y.size(-1))
+
+        return y
+
+
+class View(nn.Module):
+    def __init__(self, *shape: int):
+        super().__init__()
+        self.shape = shape
+
+    def forward(self, x_in: torch.Tensor) -> torch.Tensor:
+        return x_in.view(self.shape)
+
+
+class Permute(nn.Module):
+    def __init__(self, *dimensions: int):
+        super().__init__()
+        self.dimensions = dimensions
+
+    def forward(self, x_in: torch.Tensor) -> torch.Tensor:
+        return x_in.permute(self.dimensions)
