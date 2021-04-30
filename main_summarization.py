8e54075a55c9e275cbb4d2ed5b733abd316a004c
Mikołaj Soska <miki_2007@gazeta.pl>
2021-04-30 08:59:44 +0200

    Fixes and improvements in train loop.

--- main_summarization.py
+++ main_summarization.py
@@ -1,3 +1,4 @@
+import gc
 import time
 
 import torch
@@ -8,10 +9,11 @@ from model.text_summarization.pointer_generator import PointerGeneratorNetwork
 from utils import set_random_seed
 
 device = 'cuda'
-load_checkpoint = False
+load_checkpoint = True
 set_random_seed(0)
-epochs = 1
-batch_size = 16
+epochs = 5
+batch_size = 4
+
 dataset = SummarizationDataset('cnn_dailymail', max_article_length=400, max_summary_length=100)
 loader = SummarizationDataLoader(dataset, batch_size=batch_size)
 model = PointerGeneratorNetwork(dataset.get_vocab_size())
@@ -22,13 +24,29 @@ criterion_coverage = utils.CoverageLoss()
 optimizer = torch.optim.Adagrad(model.parameters(), lr=0.15, initial_accumulator_value=0.1)
 
 if load_checkpoint:
-    checkpoint = torch.load('data/weights/summarization-model-cpu.pt')
+    print('Loading checkpoint...')
+    checkpoint = torch.load('data/weights/summarization-model.pt')
     model.load_state_dict(checkpoint['model_state_dict'])
     optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    epoch_start = checkpoint['epoch']
+    previous_batch_size = checkpoint['batch_size']
+    iteration = checkpoint['iteration'] * previous_batch_size // batch_size
+    print(f'Epoch set to {epoch_start}. Iteration set to {iteration}.')
+    del checkpoint
+else:
+    iteration = 0
+    epoch_start = 0
 
+gc.collect()
 time_start = time.time()
-for epoch in range(epochs):
+running_loss = []
+for epoch in range(epoch_start, epochs):
     for i, (texts, texts_lengths, summaries, summaries_lengths, targets) in enumerate(loader):
+        if i < iteration:
+            continue
+        else:
+            iteration = 0
+
         optimizer.zero_grad(set_to_none=True)
         torch.cuda.empty_cache()
         texts = texts.to(device=device)
@@ -41,16 +59,20 @@ for epoch in range(epochs):
         loss = criterion(output, targets, summaries_lengths) + criterion_coverage(attention, coverage, targets)
         loss.backward()
         optimizer.step()
+        running_loss.append(loss.item())
 
-        if i % 10 == 1:
+        if i % 50 == 1:
             time_iter = round(time.time() - time_start, 2)
-            memory = round(torch.cuda.memory_reserved(0) / (1024 ** 3), 2)  # To GB
-            print(f'Epoch: {epoch} Iter: {i}/{len(loader)} Loss: {loss}, Time: {time_iter} seconds, '
-                  f'Memory: {memory} GB')
             torch.save({
                 'epoch': epoch,
+                'iteration': i,
+                'batch_size': batch_size,
                 'model_state_dict':
                     model.state_dict(),
                 'optimizer_state_dict': optimizer.state_dict()
             }, 'data/weights/summarization-model.pt')
             time_start = time.time()
+            memory = round(torch.cuda.memory_reserved(0) / (1024 ** 3), 2)  # To GB
+            loss = sum(running_loss) / len(running_loss)
+            print(f'Epoch: {epoch} Iter: {i}/{len(loader)} Loss: {loss}, Time: {time_iter} seconds, '
+                  f'Memory: {memory} GB')
