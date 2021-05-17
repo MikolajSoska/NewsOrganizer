497b2862e19485cd67e0128bef62892dd29f6c8e
Mikołaj Soska <miki_2007@gazeta.pl>
2021-05-17 19:18:20 +0200

    Added base abstract class for news parsers.

---
+++ news/parsers/base.py
@@ -0,0 +1,21 @@
+import abc
+from typing import List, Optional
+
+import bs4
+import requests
+
+
+class BaseNewsParser(abc.ABC):
+    @abc.abstractmethod
+    def parse_article_content(self, url: str) -> Optional[List[str]]:
+        pass
+
+    @staticmethod
+    def __get_article_site(url: str) -> Optional[bs4.BeautifulSoup]:
+        response = requests.get(url)
+        if response.status_code == requests.codes.ok:
+            return bs4.BeautifulSoup(response.content, 'html.parser')
+        else:
+            print(f'HTTP {response.status_code} code when accessing article to parse. Error message: {response.text}.'
+                  f' Article URL: {url}')
+            return None
--- news/parsers/cnbc.py
+++ news/parsers/cnbc.py
@@ -1,11 +1,10 @@
 import re
 from typing import List, Optional
 
-import bs4
-import requests
+from news.parsers.base import BaseNewsParser
 
 
-class CNBCParser:
+class CNBCParser(BaseNewsParser):
     def parse_article_content(self, url: str) -> Optional[List[str]]:
         article_site = self.__get_article_site(url)
         if article_site is None:
@@ -18,13 +17,3 @@ class CNBCParser:
                 article.append(text.text)
 
         return article
-
-    @staticmethod
-    def __get_article_site(url: str) -> Optional[bs4.BeautifulSoup]:
-        response = requests.get(url)
-        if response.status_code == requests.codes.ok:
-            return bs4.BeautifulSoup(response.content, 'html.parser')
-        else:
-            print(f'HTTP {response.status_code} code when accessing article to parse. Error message: {response.text}.'
-                  f' Article URL: {url}')
-            return None
