58d0c26bc4a531bbe8a3cc20647160e760032790
Mikołaj Soska <miki_2007@gazeta.pl>
2021-07-19 17:09:09 +0200

    Added CoNLL-2003 dataset to DatasetGenerator for NER models.

--- neural/common/datasets.py
+++ neural/common/datasets.py
@@ -14,6 +14,8 @@ class DatasetGenerator:
     def generate_dataset(self) -> Iterator[Tuple[Any, ...]]:
         if self.__dataset_name == 'cnn_dailymail':
             return self.__generate_cnn_dailymail()
+        elif self.__dataset_name == 'conll2003':
+            return self.__generate_conll2003()
         else:
             raise ValueError(f'Dataset "{self.__dataset_name}" is not supported.')
 
@@ -28,3 +30,12 @@ class DatasetGenerator:
             text_tokens = tokenizer(text)
             summary_tokens = tokenizer(summary)
             yield text_tokens, summary_tokens
+
+    def __generate_conll2003(self) -> Iterator[Tuple[List[str], List[str]]]:
+        dataset = datasets.load_dataset('conll2003', split=self.__split)
+        dataset = dataset.to_dict()
+        tokens_list = dataset['tokens']
+        tags_list = dataset['ner_tags']
+
+        for tokens, tags in tqdm.tqdm(zip(tokens_list, tags_list), total=len(tokens_list), file=sys.stdout):
+            yield tokens, tags
