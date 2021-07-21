c0f38214198eaf2a8b4261e8c72cb005c6ac558f
Mikołaj Soska <miki_2007@gazeta.pl>
2021-07-21 21:48:47 +0200

    Updated BiLSTMConv model with more configurable parameters. Added additional word and char features to model.

--- neural/ner/bilstm_cnn.py
+++ neural/ner/bilstm_cnn.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 import torch
 import torch.nn as nn
 from torch import Tensor
@@ -6,60 +8,92 @@ import neural.common.layers as layers
 
 
 class BiLSTMConv(nn.Module):
-    def __init__(self, embeddings: Tensor, output_size: int, batch_size: int, char_count: int,
-                 max_word_length: int, char_embedding_size: int):
-        super().__init__()
-        self.char_features_size = 50
-        self.hidden_size = 50
-        self.batch_size = batch_size
+    def __init__(self, output_size: int, conv_width: int, conv_output_size: int, hidden_size: int, lstm_layers: int,
+                 dropout_rate: float, char_vocab_size: int, char_embedding_dim: int, word_vocab_size: int = None,
+                 word_embedding_dim: int = None, embeddings: Tensor = None, use_word_features: bool = False,
+                 use_char_features: bool = False):
+        if embeddings is None and (word_embedding_dim is None or word_vocab_size is None):
+            raise ValueError('Either embeddings vector or embedding dim and vocab size must be passed.')
 
-        char_conv = nn.Conv1d(char_embedding_size, self.char_features_size, kernel_size=3)
-        conv_len_out = self.__get_conv_length_out(max_word_length, char_conv)
-        self.char_network = nn.Sequential(
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.lstm_layers = lstm_layers
+
+        self.char_embedding = nn.Sequential(
             nn.Flatten(start_dim=0, end_dim=1),
-            nn.Embedding(char_count, char_embedding_size, padding_idx=0),
-            layers.Permute(0, 2, 1),
-            char_conv,
-            nn.MaxPool1d(conv_len_out),
-            layers.View(-1, self.batch_size, self.char_features_size)
+            nn.Embedding(char_vocab_size, char_embedding_dim, padding_idx=0),
         )
 
-        self.word_embedding = nn.Embedding.from_pretrained(embeddings, freeze=False, padding_idx=0)
-        self.lstm = nn.LSTM(input_size=50 + self.char_features_size, hidden_size=self.hidden_size, num_layers=1,
-                            bidirectional=True)
+        if use_char_features:
+            self.char_features = nn.Sequential(
+                nn.Flatten(start_dim=0, end_dim=1),
+                nn.Embedding(num_embeddings=5, embedding_dim=4, padding_idx=0)
+            )
+            char_embedding_dim += 4  # Add size for additional char features
+        else:
+            self.char_features = None
+
+        self.char_nn = nn.Sequential(
+            layers.Permute(0, 2, 1),
+            nn.Conv1d(char_embedding_dim, conv_output_size, kernel_size=conv_width),
+            layers.Max(dimension=-1)
+        )
+
+        if embeddings is not None:
+            self.embedding = nn.Embedding.from_pretrained(embeddings, freeze=False, padding_idx=0)
+        else:
+            self.embedding = nn.Embedding(word_vocab_size, word_embedding_dim, padding_idx=0)
+
+        if use_word_features:
+            self.word_features = nn.Embedding(num_embeddings=6, embedding_dim=5, padding_idx=0)
+            conv_output_size += 5  # Add size for additional word features
+        else:
+            self.word_features = None
+
+        self.lstm = nn.LSTM(input_size=self.embedding.embedding_dim + conv_output_size, hidden_size=hidden_size,
+                            num_layers=lstm_layers, bidirectional=True, dropout=dropout_rate if lstm_layers > 1 else 0)
 
         self.forward_out = layers.TimeDistributed(nn.Sequential(
+            nn.Dropout(p=dropout_rate),
             nn.Linear(self.hidden_size, output_size),
             nn.LogSoftmax(dim=1)
         ))
+
         self.backward_out = layers.TimeDistributed(nn.Sequential(
+            nn.Dropout(p=dropout_rate),
             nn.Linear(self.hidden_size, output_size),
             nn.LogSoftmax(dim=1)
         ))
 
-        self.hidden = self.__init_hidden()
+    def __init_hidden(self, batch_size: int, device: str) -> Tuple[Tensor, Tensor]:
+        hidden_shape = (2 * self.lstm_layers, batch_size, self.hidden_size)
+        return torch.zeros(hidden_shape, device=device), torch.zeros(hidden_shape, device=device)
 
-    def __init_hidden(self):
-        return (torch.randn(2, self.batch_size, self.hidden_size, device='cuda'),
-                torch.randn(2, self.batch_size, self.hidden_size, device='cuda'))
+    def forward(self, sentences_in: Tensor, chars_in: Tensor, word_features_in: Tensor,
+                char_features_in: Tensor) -> Tensor:
+        sequence_length, batch_size = sentences_in.shape
 
-    @staticmethod
-    def __get_conv_length_out(input_length: int, conv: nn.Conv1d) -> int:
-        return int((input_length + 2 * conv.padding[0] - conv.dilation[0] *
-                    (conv.kernel_size[0] - 1) - 1) / conv.stride[0] + 1)
+        char_features = self.char_embedding(chars_in)
+        if self.char_features is not None:
+            additional_features = self.char_features(char_features_in)
+            char_features = torch.cat((char_features, additional_features), dim=-1)
 
-    def forward(self, sentences_in: Tensor, chars_in: Tensor) -> Tensor:
-        x_chars = self.char_network(chars_in)
+        char_features = self.char_nn(char_features)
+        char_features = char_features.view((sequence_length, batch_size, -1))
 
-        self.hidden = self.__init_hidden()
-        x_sentences = self.word_embedding(sentences_in)
-        x_sentences = torch.cat([x_sentences, x_chars], dim=-1)
+        sentences = self.embedding(sentences_in)
+        if word_features_in is not None:
+            additional_features = self.word_features(word_features_in)
+            sentences = torch.cat((sentences, additional_features, char_features), dim=-1)
+        else:
+            sentences = torch.cat((sentences, char_features), dim=-1)
 
-        x_sentences, self.hidden = self.lstm(x_sentences, self.hidden)
-        x_forward = x_sentences[:, :, :self.hidden_size]
-        x_backward = x_sentences[:, :, self.hidden_size:]
+        hidden, cell = self.__init_hidden(batch_size, sentences_in.device)
+        sentences, _ = self.lstm(sentences, (hidden, cell))
+        forward = sentences[:, :, :self.hidden_size]
+        backward = sentences[:, :, self.hidden_size:]
 
-        x_forward = self.forward_out(x_forward)
-        x_backward = self.backward_out(x_backward)
+        forward = self.forward_out(forward)
+        backward = self.backward_out(backward)
 
-        return x_forward + x_backward
+        return forward + backward
