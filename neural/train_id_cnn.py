b313eb5b7cd165e3a70b7751130eef5c16670ffb
Mikołaj Soska <miki_2007@gazeta.pl>
2021-08-16 19:32:33 +0200

    Added module for training ID-CNN model.

--- neural/train_id_cnn.py
+++ neural/train_id_cnn.py
@@ -14,28 +14,30 @@ from neural.common.scores import Precision, Recall, F1Score
 from neural.common.scores import ScoreValue
 from neural.common.train import Trainer, add_base_train_args
 from neural.common.utils import set_random_seed, dump_args_to_file
-from neural.ner.bilstm_cnn import BiLSTMConv
 from neural.ner.dataloader import NERDataset, NERDataLoader
+from neural.ner.id_cnn import IteratedDilatedCNN
 from utils.database import DatabaseConnector
 
 
 def parse_args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(description='Parameters for BiLSTM-CNN model training.')
+    parser = argparse.ArgumentParser(description='Parameters for ID-CNN model training.')
     parser.add_argument('--dataset', choices=['conll2003', 'gmb'], default='conll2003', help='Dataset name')
-    parser.add_argument('--epochs', type=int, default=80, help='Training epochs')
-    parser.add_argument('--batch', type=int, default=9, help='Batch size')
-    parser.add_argument('--lr', type=float, default=0.0105, help='Learning rate')
+    parser.add_argument('--epochs', type=int, default=100, help='Training epochs')
+    parser.add_argument('--batch', type=int, default=128, help='Batch size')
+    parser.add_argument('--lr', type=float, default=0.0005, help='Learning rate')
     parser.add_argument('--cnn-width', type=int, default=3, help='Convolution width')
-    parser.add_argument('--cnn-output', type=int, default=53, help='Convolution output size')
-    parser.add_argument('--lstm-state', type=int, default=275, help='LSTM hidden layers size')
-    parser.add_argument('--lstm-layers', type=int, default=1, help='Number of LSTM layers')
-    parser.add_argument('--dropout', type=float, default=0.68, help='Dropout rate')
-    parser.add_argument('--char-embedding-size', type=int, default=25, help='Size of chars embedding')
+    parser.add_argument('--cnn-filters', type=int, default=300, help='Convolution filters size')
+    parser.add_argument('--dilation', type=int, default=[1, 2, 1], nargs='+', help='Dilation sizes in CNN block')
+    parser.add_argument('--block-repeat', type=int, default=1, help='Convolution block repeats number')
+    parser.add_argument('--input-dropout', type=float, default=0.65, help='Dropout rate for inputs')
+    parser.add_argument('--block-dropout', type=float, default=0.85, help='Dropout rate after each CNN block')
     parser.add_argument('--pretrained-embeddings', choices=['no', 'collobert', 'glove'], default='collobert',
                         help='Which pretrained embeddings use')
-    parser.add_argument('--word-embedding-size', type=int, default=50, help='Size of word embedding (if no pretrained')
+    parser.add_argument('--word-embedding-size', type=int, default=100, help='Size of word embedding (if no pretrained')
     parser.add_argument('--word-features', action='store_true', help='Use additional word features')
-    parser.add_argument('--char-features', action='store_true', help='Use additional char features')
+    parser.add_argument('--max-gradient-norm', type=int, default=5, help='Max norm for gradient clipping')
+    parser.add_argument('--adam-betas', type=float, default=[0.9, 0.9], nargs=2, help='Betas for Adam optimizer')
+    parser.add_argument('--adam-eps', type=float, default=1e-6, help='Epsilon value for Adam optimizer')
     parser.add_argument('--embedding-path', type=Path, default='../data/saved/embeddings', help='Path to embeddings')
     parser = add_base_train_args(parser)
 
@@ -43,40 +45,42 @@ def parse_args() -> argparse.Namespace:
 
 
 def train_step(trainer: Trainer, inputs: Tuple[Any, ...]) -> Tuple[Tensor, ScoreValue]:
-    words, tags, chars, word_features, char_features = inputs
-    model = trainer.model.bilstm_cnn
+    words, tags, _, word_features, _ = inputs
+    model = trainer.model.id_cnn
 
-    output = model(words, chars, word_features, char_features)
-    output = torch.flatten(output, end_dim=1)
+    outputs = model(words, word_features)
     tags = torch.flatten(tags)
 
-    # Remove padding from tags and output
-    output = output[tags >= 0]
-    tags = tags[tags >= 0]
+    loss = torch.tensor(0., device=words.device)
+    score = ScoreValue()
+    tags_without_padding = tags[tags >= 0]
+    for output in outputs:
+        output = torch.flatten(output, end_dim=1)
+        output = output[tags >= 0]
+        loss += trainer.criterion.cross_entropy(output, tags_without_padding)
+        score += trainer.score(output, tags_without_padding)
 
-    loss = trainer.criterion.cross_entropy(output, tags)
-    score = trainer.score(output, tags)
-    del output
+    loss = loss / len(outputs)
+    score = score / len(outputs)
+    del outputs
 
     return loss, score
 
 
 def create_model_from_args(args: argparse.Namespace, tags_count: int, vocab: VocabWithChars,
-                           embeddings: Tensor = None) -> BiLSTMConv:
-    return BiLSTMConv(
+                           embeddings: Tensor = None) -> IteratedDilatedCNN:
+    return IteratedDilatedCNN(
         output_size=tags_count,
         conv_width=args.cnn_width,
-        conv_output_size=args.cnn_output,
-        hidden_size=args.lstm_state,
-        lstm_layers=args.lstm_layers,
-        dropout_rate=args.dropout,
-        char_vocab_size=len(vocab.chars),
-        char_embedding_dim=args.char_embedding_size,
-        word_vocab_size=len(vocab),
-        word_embedding_dim=args.word_embedding_size,
+        conv_filters=args.cnn_filters,
+        dilation_sizes=args.dilation,
+        block_repeats=args.block_repeat,
+        input_dropout=args.input_dropout,
+        block_dropout=args.block_dropout,
+        vocab_size=len(vocab),
+        embedding_dim=args.word_embedding_size,
         embeddings=embeddings,
         use_word_features=args.word_features,
-        use_char_features=args.char_features
     )
 
 
@@ -84,7 +88,7 @@ def main() -> None:
     args = parse_args()
     set_random_seed(args.seed)
 
-    model_name = 'bilstm_cnn'
+    model_name = 'id_cnn'
     dump_args_to_file(args, args.model_path / model_name)
     vocab = VocabBuilder.build_vocab(args.dataset, 'ner', vocab_type='char', vocab_dir=args.vocab_path,
                                      digits_to_zero=True)
@@ -116,7 +120,7 @@ def main() -> None:
     trainer = Trainer(
         train_step=train_step,
         epochs=args.epochs,
-        max_gradient_norm=None,
+        max_gradient_norm=args.max_gradient_norm,
         model_save_path=args.model_path,
         log_save_path=args.logs_path,
         model_name=model_name,
@@ -125,15 +129,15 @@ def main() -> None:
         scores=[Precision(), Recall(), F1Score()]
     )
     trainer.set_models(
-        bilstm_cnn=model
+        id_cnn=model
     )
     trainer.set_criterion(
         cross_entropy=nn.CrossEntropyLoss()
     )
     trainer.set_optimizer(
-        sgd=torch.optim.SGD(model.parameters(), lr=args.lr)
+        adam=torch.optim.Adam(model.parameters(), lr=args.lr, betas=tuple(args.adam_betas), eps=args.adam_eps)
     )
-    trainer.train(train_loader, validation_loader, verbosity=500, save_interval=500)
+    trainer.train(train_loader, validation_loader, verbosity=50, save_interval=50)
     trainer.eval(test_loader)
 
 
