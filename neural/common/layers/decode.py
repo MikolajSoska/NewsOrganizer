2eeaf0e04e63dce5dc3f0343cff9452c5721c38e
Mikołaj Soska <miki_2007@gazeta.pl>
2021-08-27 11:38:39 +0200

    Added base class for RNN decoder.

--- neural/common/layers/decode.py
+++ neural/common/layers/decode.py
@@ -1,3 +1,6 @@
+from abc import ABC, abstractmethod
+from typing import List, Tuple, Any, Optional
+
 import torch
 import torch.nn as nn
 from torch import Tensor
@@ -78,3 +81,68 @@ class CRF(nn.Module):
         result = pad_sequence(best_tags_list, padding_value=-1)
 
         return result
+
+
+class BaseRNNDecoder(nn.Module, ABC):
+    def __init__(self, bos_index: int, max_output_length: int, embedding: nn.Embedding):
+        super().__init__()
+        self.bos_index = bos_index
+        self.max_output_length = max_output_length
+        self.embedding = embedding
+
+    def __validate_outputs(self, outputs: Optional[Tensor], teacher_forcing_ratio: float, batch_size: int,
+                           device: str) -> Tuple[Tensor, float]:
+        if self.training:
+            if outputs is None:
+                if teacher_forcing_ratio > 0:
+                    raise AttributeError('During training with teacher forcing reference summaries must be provided.')
+                else:
+                    outputs = torch.full((self.max_output_length, batch_size), self.bos_index, dtype=torch.long,
+                                         device=device)
+        else:  # In validation phase never use passed summaries
+            outputs = torch.full((self.max_output_length, batch_size), self.bos_index, dtype=torch.long, device=device)
+            teacher_forcing_ratio = 0.  # In validation phase teacher forcing is not used
+
+        return outputs, teacher_forcing_ratio
+
+    def forward(self, outputs: Optional[Tensor], teacher_forcing_ratio: float, batch_size: int, device: str,
+                cyclic_inputs: Tuple[Any, ...], constant_inputs: Tuple[Any, ...]) -> Tuple[Tensor, Tensor,
+                                                                                           List[Tuple[Any, ...]]]:
+        outputs, teacher_forcing_ratio = self.__validate_outputs(outputs, teacher_forcing_ratio, batch_size, device)
+        decoder_input = outputs[0, :]
+
+        predictions = []
+        predicted_tokens = []
+        decoder_outputs = []
+        for i in range(self.max_output_length):
+            decoder_input = self.embedding(decoder_input)
+            prediction, cyclic_inputs, decoder_out = self.decoder_step(decoder_input, cyclic_inputs, constant_inputs)
+            predictions.append(prediction)
+            decoder_outputs.append(decoder_out)
+
+            tokens = self.get_new_decoder_inputs(prediction)
+            tokens = tokens.detach()
+            predicted_tokens.append(tokens)
+
+            if i + 1 < self.max_output_length:
+                if teacher_forcing_ratio <= 1:
+                    use_predictions = torch.as_tensor(torch.rand(batch_size, device=device) >= teacher_forcing_ratio)
+                    # Depending on the value of `use_predictions` in next step decoder will use predicted tokens or
+                    # ground truth values (teacher forcing)
+                    decoder_input = torch.where(use_predictions, tokens, outputs[i + 1, :])
+                else:  # If teacher forcing is used in every step
+                    decoder_input = outputs[i + 1, :]
+
+        predictions = torch.stack(predictions)
+        predicted_tokens = torch.stack(predicted_tokens)
+
+        return predictions, predicted_tokens, decoder_outputs
+
+    @abstractmethod
+    def decoder_step(self, decoder_input: Tensor, cyclic_inputs: Tuple[Any, ...],
+                     constant_inputs: Tuple[Any, ...]) -> Tuple[Tensor, Tuple[Any, ...], Tuple[Any, ...]]:
+        pass
+
+    @abstractmethod
+    def get_new_decoder_inputs(self, predictions: Tensor) -> Tensor:
+        pass
