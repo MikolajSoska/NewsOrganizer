85512a85bd038fa07bbd807e444a48512efeeca7
Mikołaj Soska <miki_2007@gazeta.pl>
2021-09-28 12:14:03 +0200

    Added base files for website header.

---
+++ web_app/static/scripts/header.js
@@ -0,0 +1,9 @@
+function changeNerModel() {
+    const nerSelect = document.getElementById("ner-select");
+    console.log(nerSelect.value)
+}
+
+function changeSummarizationModel() {
+    const summarizationSelect = document.getElementById("summarization-select");
+    console.log(summarizationSelect.value)
+}
